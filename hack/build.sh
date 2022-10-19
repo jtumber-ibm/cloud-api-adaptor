3c669df3eeaf456161d8eb3faaa1eae1c01c2a36
James Tumber <james.tumber@ibm.com>
2022-10-19 13:30:46 +0100

    test: Allow s390x docker builds

--- hack/build.sh
+++ hack/build.sh
@@ -8,56 +8,36 @@ script_dir=$(dirname "$(readlink -f "$0")")
 
 registry="${registry:-quay.io/confidential-containers/cloud-api-adaptor-${CLOUD_PROVIDER}}"
 
-supported_arches=(
-	"linux/amd64"
-)
-
-function setup_env_for_arch() {
-	case "$1" in
-		"linux/amd64")
-			kernel_arch="x86_64"
-			;;
-		(*) echo "$1 is not supported" && exit 1
-	esac
-}
+if [[ "$CLOUD_PROVIDER" == "ibmcloud" ]]; then
+	supported_arches=(
+        "linux/amd64"
+        "linux/s390x"
+    )
+else
+	supported_arches=(
+        "linux/amd64"
+    )
+fi
 
 function build_caa_payload() {
-	pushd "${script_dir}/.."
+    pushd "${script_dir}/.."
 
-	tag=$(date +%Y%m%d%H%M%s)
+    tag=$(date +%Y%m%d%H%M%s)
 
-	for arch in ${supported_arches[@]}; do
-		setup_env_for_arch "${arch}"
+    platform=$(IFS=, ; echo "${supported_arches[*]}")
 
-		echo "Building cloud-api-adaptor ${CLOUD_PROVIDER} image for ${arch}"
-		docker buildx build \
-			--build-arg ARCH="${kernel_arch}" \
-			--build-arg CLOUD_PROVIDER="${CLOUD_PROVIDER}" \
-			-f Dockerfile \
-			-t "${registry}:${kernel_arch}-${tag}" \
-			--platform="${arch}" \
-			--load \
-			.
-		docker push "${registry}:${kernel_arch}-${tag}"
-	done
+    docker buildx build \
+        --push \
+        --platform ${platform} \
+        --tag "${registry}:${tag}" --tag "${registry}:latest" -f Dockerfile \
+        .
 
-	docker manifest create \
-		${registry}:${tag} \
-		--amend ${registry}:x86_64-${tag}
-
-	docker manifest create \
-		${registry}:latest \
-		--amend ${registry}:x86_64-${tag}
-
-	docker manifest push ${registry}:${tag}
-	docker manifest push --purge ${registry}:latest
-
-	popd
+    popd
 }
 
 function main() {
-	build_caa_payload
-	# TODO: kustomize with the tag
+    build_caa_payload
+    # TODO: kustomize with the tag
 }
 
 main "$@"
--- install/pre-install-payload/hack/build.sh
+++ install/pre-install-payload/hack/build.sh
@@ -10,50 +10,23 @@ registry="${registry:-quay.io/confidential-containers/peer-pods-pre-install-payl
 
 supported_arches=(
 	"linux/amd64"
+	"linux/s390x"
 )
 
-function setup_env_for_arch() {
-	case "$1" in
-		"linux/amd64") 
-			image="docker.io/library/centos"
-			kernel_arch="x86_64"
-			;;
-		(*) echo "$1 is not supported" && exit 1
-	esac
-		
-}
-
 function build_preinstall_payload() {
-	pushd "${script_dir}/.."
+    pushd "${script_dir}/.."
 
-	tag=$(date +%Y%m%d%H%M%s)
+    tag=$(date +%Y%m%d%H%M%s)
 
-	for arch in ${supported_arches[@]}; do
-		setup_env_for_arch "${arch}"
+    platform=$(IFS=, ; echo "${supported_arches[*]}")
 
-		echo "Building payload image for ${arch}"
-		docker buildx build \
-			--build-arg ARCH="${kernel_arch}" \
-			-f Dockerfile \
-			-t "${registry}:${kernel_arch}-${tag}" \
-			--platform="${arch}" \
-			--load \
-			.
-		docker push "${registry}:${kernel_arch}-${tag}"
-	done
+    docker buildx build \
+        --push \
+        --platform ${platform} \
+        --tag "${registry}:${tag}" --tag "${registry}:latest" -f Dockerfile \
+        .
 
-	docker manifest create \
-		${registry}:${tag} \
-		--amend ${registry}:x86_64-${tag}
-
-	docker manifest create \
-		${registry}:latest \
-		--amend ${registry}:x86_64-${tag}
-
-	docker manifest push ${registry}:${tag}
-	docker manifest push --purge ${registry}:latest
-
-	popd
+    popd
 }
 
 function main() {
